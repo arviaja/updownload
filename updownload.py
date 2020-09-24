09fddafc02f115064e8becd0e7ce35f379aea78c
Mark Beinker <Mark.Beinker@dentsplysirona.com>
2020-09-24 15:42:32 +0200

    argparse added, main function introduced, clean up of remote files added

--- updownload.py
+++ updownload.py
@@ -1,51 +1,64 @@
 #!/usr/bin/env python3
 
+import argparse
 from paramiko import SSHClient
 from scp import SCPClient
 import sys, glob, os
 from timeit import default_timer as timer
 
-hostname = sys.argv[1]
-key_file = sys.argv[2]
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('hostname', help='remote machine to up- and download test files')
+    parser.add_argument('-u', '--user', default=None, help='name of remote user')
+    parser.add_argument('-k', '--key-file', default=None, help='path of private ssh key file')  
+    args = parser.parse_args()
 
-filesize = 153600
-fileamount = 48
-fileprefix = 'outputfile_'
+    hostname = args.hostname
+    user = args.user
+    key_file = args.key_file
 
-print ('Filesize is', filesize)
-print ('amount of generated files:', fileamount)
-print ('fileprefix is:', fileprefix)
+    filesize = 153600
+    fileamount = 24
+    fileprefix = 'outputfile_'
 
-
-# Generate temporary test files with fixed size and random content
-print ('Generating random files...')
-for i in range (0, fileamount):
-    filename = fileprefix+str(i+1)
-    with open(filename, 'wb') as fout:
-        fout.write(os.urandom(filesize))
-
-
-# Define SSH connection to remote server. Remote host is defined by cmd-line input
-ssh = SSHClient()
-ssh.load_system_host_keys()
-ssh.connect(hostname, username='azureuser', key_filename=key_file)
-
-# Open SSH-Socket
-with SCPClient(ssh.get_transport()) as scp:
-    start = timer()
-    # loop through files. Upload, then download
-    for i in range(1,fileamount):
-        file = fileprefix+str(i)
-        scp.put(file)
-        scp.get(file)
-    end = timer()
-    print(end - start)
+    print ('Filesize is', filesize)
+    print ('amount of generated files:', fileamount)
+    print ('fileprefix is:', fileprefix)
 
 
-# cleanup
-fileList = glob.glob(fileprefix+'*')
-for filePath in fileList:
-    try:
-        os.remove(filePath)
-    except:
-        print("Error while deleting file : ", filePath)
+    # Generate temporary test files with fixed size and random content
+    print ('Generating random files...')
+    for i in range (0, fileamount):
+        filename = fileprefix+str(i+1)
+        with open(filename, 'wb') as fout:
+            fout.write(os.urandom(filesize))
+
+
+    # Define SSH connection to remote server. Remote host is defined by cmd-line input
+    ssh = SSHClient()
+    ssh.load_system_host_keys()
+    ssh.connect(hostname, username=user, key_filename=key_file)
+
+    # Open SSH-Socket
+    with SCPClient(ssh.get_transport()) as scp:
+        start = timer()
+        # loop through files. Upload, then download
+        for i in range(1,fileamount):
+            file = fileprefix+str(i)
+            scp.put(file)
+            scp.get(file)
+        end = timer()
+        print(end - start)
+
+
+    # cleanup
+    fileList = glob.glob(fileprefix+'*')
+    for filePath in fileList:
+        try:
+            os.remove(filePath)
+            ssh.exec_command('rm %s' % filePath)
+        except:
+            print("Error while deleting file : ", filePath)
+
+if __name__=="__main__":
+    main()
