cdabf9954f7cd0bf6bb9ca59810dd0b537218c2a
Mark Beinker <Mark.Beinker@dentsplysirona.com>
2020-09-24 13:58:12 +0200

    adjustments for use with azure VM

--- updownload.py
+++ updownload.py
@@ -5,9 +5,11 @@ from scp import SCPClient
 import sys, glob, os
 from timeit import default_timer as timer
 
-hostname = str(sys.argv[1])
+hostname = sys.argv[1]
+key_file = sys.argv[2]
+
 filesize = 153600
-fileamount = 24
+fileamount = 48
 fileprefix = 'outputfile_'
 
 print ('Filesize is', filesize)
@@ -26,14 +28,14 @@ for i in range (0, fileamount):
 # Define SSH connection to remote server. Remote host is defined by cmd-line input
 ssh = SSHClient()
 ssh.load_system_host_keys()
-ssh.connect(hostname)
+ssh.connect(hostname, username='azureuser', key_filename=key_file)
 
 # Open SSH-Socket
 with SCPClient(ssh.get_transport()) as scp:
     start = timer()
     # loop through files. Upload, then download
-    for i in range(0,23):
-        file = fileprefix+str(i+1)
+    for i in range(1,fileamount):
+        file = fileprefix+str(i)
         scp.put(file)
         scp.get(file)
     end = timer()
